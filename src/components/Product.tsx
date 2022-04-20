ecaa0f365afcdb4a5844755d829344c792fa03e2
Shea Meyers <shea.meyers@gmail.com>
2022-04-20 21:19:13 +0200

    Improve spacing

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -2,10 +2,11 @@ import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import type { ProductType } from "../types";
+import "../styles/Product.css";
 
 const Product = (props: ProductType) => {
   return (
-    <Card variant="outlined">
+    <Card variant="outlined" className="Product">
       <CardContent>
         <Avatar alt={`${props.title} Image`} src={`${props.image}`} />
         <h3>{props.title}</h3>
